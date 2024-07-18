22ea6c4b8266b552fd32c615ae0c7e623bcd7206
Carlos Daniel <carlos.daniel.0486@gmail.com>
2024-07-18 04:32:48 -0300

    style: adjusting indentation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,28 @@
+import * as Dialog from "@radix-ui/react-dialog";
+
+import { NewTransactionModal } from "../NewTransactionModal";
+
+import logo from "../../assets/logo.svg";
+
 import {
   HeaderContainer,
   HeaderContent,
   NewTransactionsButton,
 } from "./styles";
 
-import logo from "../../assets/logo.svg";
-
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logo} />
-        <NewTransactionsButton>Nova transação</NewTransactionsButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionsButton>Nova transação</NewTransactionsButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
