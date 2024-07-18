ee978862e0e6f5c209fa42b94a2efdd16c95a1c3
Carlos Daniel <carlos.daniel.0486@gmail.com>
2024-07-18 10:04:54 -0300

    refactor: adjusting style props

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -82,7 +82,7 @@ export const TransactionType = styled(RadioGroup.Root)`
 `;
 
 interface TransactionTypeButtonProps {
-  variant: "income" | "outcome";
+  $variant: "income" | "outcome";
 }
 
 export const TransactionTypeButton = styled(
@@ -103,7 +103,7 @@ export const TransactionTypeButton = styled(
 
   svg {
     color: ${(props) =>
-      props.variant === "income"
+      props.$variant === "income"
         ? props.theme["green-300"]
         : props.theme["red-300"]};
   }
@@ -115,7 +115,7 @@ export const TransactionTypeButton = styled(
   &[data-state="checked"] {
     color: ${(props) => props.theme.white};
     background: ${(props) =>
-      props.variant === "income"
+      props.$variant === "income"
         ? props.theme["green-500"]
         : props.theme["red-500"]};
 
