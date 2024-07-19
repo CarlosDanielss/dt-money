5f23ecf8e4ddd0c5a51258e45bc51f876ed6a5c1
Carlos Daniel <carlos.daniel.0486@gmail.com>
2024-07-19 14:09:25 -0300

    feat: add function for create transactions

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
+
 import { api } from "../lib/axios";
 
 interface TransactionType {
@@ -10,9 +11,17 @@ interface TransactionType {
   createdAt: string;
 }
 
+interface CreateTransactionsType {
+  description: string;
+  price: number;
+  category: string;
+  type: "income" | "outcome";
+}
+
 interface TransactionContextType {
   transactions: TransactionType[];
   fetchTransactions: (query?: string) => Promise<void>;
+  createTransactions: (data: CreateTransactionsType) => Promise<void>;
 }
 
 export const TransactionsContext = createContext({} as TransactionContextType);
@@ -34,12 +43,28 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     setTransactions(response.data);
   }
 
+  async function createTransactions(data: CreateTransactionsType) {
+    const { description, category, price, type } = data;
+
+    const response = await api.post("transactions", {
+      description,
+      category,
+      price,
+      type,
+      createdAt: new Date(),
+    });
+
+    setTransactions((prev) => [...prev, response.data]);
+  }
+
   useEffect(() => {
     fetchTransactions();
   }, []);
 
   return (
-    <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
+    <TransactionsContext.Provider
+      value={{ transactions, fetchTransactions, createTransactions }}
+    >
       {children}
     </TransactionsContext.Provider>
   );
