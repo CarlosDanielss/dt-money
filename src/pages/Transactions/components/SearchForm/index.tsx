88f5b66acd2b022061f5cde3af664898fa83d456
Carlos Daniel <carlos.daniel.0486@gmail.com>
2024-07-19 09:41:01 -0300

    feat: add validation and control in form search

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,12 +1,38 @@
 import { MagnifyingGlass } from "phosphor-react";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { SearchFormContainer } from "./styles";
 
-export function SearchForm() {
-  return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque por transações" />
+const searchFormSchema = z.object({
+  query: z.string(),
+});
 
-      <button type="submit">
+type SearchFormInputType = z.infer<typeof searchFormSchema>;
+
+export function SearchForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputType>({
+    resolver: zodResolver(searchFormSchema),
+  });
+
+  function handleSearchTransactions(data: SearchFormInputType) {
+    console.log(data);
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register("query")}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20} />
         Buscar
       </button>
